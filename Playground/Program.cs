f2a320898ac915185edad025f2894fb6785ff674
Alexander Makarov <makarov.dev@gmail.com>
2016-10-19 23:48:26 +0200

    some comments in code on how to use it for self assesment

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,41 +6,45 @@
     {
         static void Main(string[] args)
         {
-            // Basic concepts:
-            AllAboutEquality.RunAndExplore();
-            ExceptionHandling.RunAndExplore();
-            DelegatesCombining.RunAndExplore();
-            BitwiseBinaryeOrVsConditionalOr.RunAndExplore();
-            EnumerableInterfaceYieldKeyword.RunAndExplore();
-            DisposeOrderInUsing.RunAndExplore();
-            ExplicitInterfaceCall.RunAndExplore();
+            // pick up one of the study cases below, go to the code and explore without running it first
+            // once you made your mind on how the code is expected to work, uncomment the study case here to run it
+            // check whether your expectation and understanding were correct ;)
 
-            // String Interning:
-            StringInterningnRefComparision.RunAndExplore();
-            StringInterningUnderstandInternPool.RunAndExplore();
+            //          Basic concepts:
+            //AllAboutEquality.RunAndExplore();
+            //ExceptionHandling.RunAndExplore();
+            //DelegatesCombining.RunAndExplore();
+            //BitwiseBinaryeOrVsConditionalOr.RunAndExplore();
+            //EnumerableInterfaceYieldKeyword.RunAndExplore();
+            //DisposeOrderInUsing.RunAndExplore();
+            //ExplicitInterfaceCall.RunAndExplore();
 
-            // Boxing-Unboxing:
-            RichterBoxingUnboxing.RunAndExplore();
-            NullableTypes.RunAndExplore();
+            //          String Interning:
+            //StringInterningnRefComparision.RunAndExplore();
+            //StringInterningUnderstandInternPool.RunAndExplore();
 
-            // Multithreading:
-            ExceptionOnThreadExperiment.RunAndExplore();
-            VolatileIsNotEnoughUseThreadBarrierExperiment.RunAndExplore();
+            //          Boxing-Unboxing:
+            //RichterBoxingUnboxing.RunAndExplore();
+            //NullableTypes.RunAndExplore();
+
+            //          Multithreading:
+            //ExceptionOnThreadExperiment.RunAndExplore();
+            //VolatileIsNotEnoughUseThreadBarrierExperiment.RunAndExplore();
             ForeachVsParallelForeach.RunAndExplore(false);
-            ForeachVsParallelForeach.RunAndExplore(true);
+            //ForeachVsParallelForeach.RunAndExplore(true);
 
-            // Cross-process Communication (when threads are not enough)
-            AppDomainCreateAndUnwrap.RunAndExplore();
-            CrossAppDomainCommunication.RunAndExplore();
+            //          Cross-process Communication (when threads are not enough)
+            //AppDomainCreateAndUnwrap.RunAndExplore();
+            //CrossAppDomainCommunication.RunAndExplore();
 
-            // Closures in Lambdas:
-            CapturedVariablesOnClosureInLambdas.RunAndExplore();
-            ClosureOnLambdasExpandLifetime.RunAndExplore();
-            ClosureVariablesNotOnValuesInLambdas.RunAndExplore();
-            ClosureOnLambdasExpandLifetime2.RunAndExplore();
+            //          Closures in Lambdas:
+            //CapturedVariablesOnClosureInLambdas.RunAndExplore();
+            //ClosureOnLambdasExpandLifetime.RunAndExplore();
+            //ClosureVariablesNotOnValuesInLambdas.RunAndExplore();
+            //ClosureOnLambdasExpandLifetime2.RunAndExplore();
 
-            // Misc:
-            LinqListMonad.RunAndExplore();
+            //          Misc:
+            //LinqListMonad.RunAndExplore();
         }
     }
 }
